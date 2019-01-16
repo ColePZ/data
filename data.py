8e4c26db616ad74f6dc43e99915b3effd171c74f
Cole Pazoid <34737903+ColePZ@users.noreply.github.com>
2019-01-16 12:34:25 +0000

    main file

---
+++ data.py
@@ -0,0 +1,22 @@
+name = ""
+age = ""
+comment = ""
+
+name2 = ""
+age2 = ""
+comment2 = ""
+
+
+name = input("Name: ")
+age = input("Age: ")
+comment = input("Any comments? ")
+
+name2 = input("Name: ")
+age2 = input("Age: ")
+comment2 = input("Any comments? ")
+
+print(name +" Is " +age+ " Years old and has the info " +comment)
+print(name2 +" Is " +age2+ " Years old and has the info " +comment2)
+
+
+
